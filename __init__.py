d6919f354c0bccb517d2481f369bc141d2c6a29e
EtienneZorrilla <zorrilla.etienne@gmail.com>
2024-01-02 14:00:43 +0100

    nouvelle présentation

--- __init__.py
+++ __init__.py
@@ -1,1 +1,8 @@
-__version__ = '0.1.1'
+""" analyse_de_trames_NMEA_GPS V0.4.1.
+
+.. moduleauthor::CHAILLOU Matthieu <matthieuchaillou17@gmail.com>
+.. moduleauthor::ZORRILLA Etienne <zorrilla.etienne@gmail.com>
+
+"""
+
+import module1
