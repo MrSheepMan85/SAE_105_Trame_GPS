38000a4ed6d5c9982d7f647b89c287f64fd69299
EtienneZorrilla <zorrilla.etienne@gmail.com>
2024-01-05 20:02:27 +0100

    version 0.2 calcule de vitesse

--- analyse_de_trames_NMEA_GPS/calcule.py
+++ analyse_de_trames_NMEA_GPS/calcule.py
@@ -5,8 +5,28 @@ Created on Thu Dec  7 10:27:15 2023
 @author: zorrilla
 """
 from analyse_de_trames_NMEA_GPS import info_trame
-def cal(info_trame):
-    
-    y = infos["nord"]
-    x = infos["est"]
+
+#### VITESSES ####
+def vit_moyenne(trajet):
+    total = 0
+    for i in trajet:
+        total+= float(i['VTG']['vit'])
+    return total/len(trajet)
+
+def vit_min(trajet):
+    min= 10000000000000000.0
+    for i in trajet:
+        vit = float(i['VTG']['vit'])
+        if vit < min:
+            min = vit
+    return min
+
+def vit_max(trajet):
+    min= -1.0
+    for i in trajet:
+        vit = float(i['VTG']['vit'])
+        if vit > min:
+            vit_max = vit
+    return vit_max
+
     
